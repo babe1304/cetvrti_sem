0f6eb9dce96675fd3614ff02921c1e29fd8ef898
babe1304 <babidominik26@gmail.com>
2022-06-11 23:11:44 +0200

    UTR labos update

--- UTR/labos4/Parser.py
+++ UTR/labos4/Parser.py
@@ -1,44 +1,53 @@
-from re import U
-
-
 ulaz = list(input())
 izlaz = list()
+znak = ""
 
-def Parser():
+def Parser():    
+    global znak
+    znak = ulaz.pop(0) if ulaz else znak
     valjanost = S()
     print("".join(izlaz))
-    print("DA" if valjanost else "NE")
+    print("DA" if valjanost and not ulaz else "NE")
 
 def S():
+    global znak
     izlaz.append("S")
-    znak = ulaz.pop(0) if ulaz else ""
     if znak == "a":
+        znak = ulaz.pop(0) if ulaz else ""
         return A() and B()
     elif znak == "b":
+        znak = ulaz.pop(0) if ulaz else ""
         return B() and A()
     else: return False
 
 def A():
+    global znak
     izlaz.append("A")
-    znak = ulaz.pop(0) if ulaz else ""
     if znak == "b":
+        znak = ulaz.pop(0) if ulaz else ""
         return C()
     elif znak == "a":
+        znak = ulaz.pop(0) if ulaz else ""
         return True
     else: return False
 
 def B():
+    global znak
     izlaz.append("B")
-    znak = ulaz.pop(0) + ulaz.pop(0) if len(ulaz) >= 2 else ""
-    if znak == "cc":
-        s = S()
-        znak = ulaz.pop(0) + ulaz.pop(0) if len(ulaz) >= 2 else ""
-        if znak == "bc" and s:
-            return True
+    if znak == "c":
+        znak = ulaz.pop(0) if ulaz else ""
+        if znak == "c":
+            znak = ulaz.pop(0) if ulaz else ""
+            s = S()
+            if not ulaz: return False   
+            znak += ulaz.pop(0)
+            if znak != "bc" and not s: return False
+            znak = ulaz.pop(0) if ulaz else ""
         else: return False
     return True
 
 def C():
+    #print(f'C: {"".join(ulaz)}')
     izlaz.append("C")
     return A() and A()
 
